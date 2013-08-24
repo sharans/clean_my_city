9c9852ce4dd6e4536962a8c720ce7d593c11cbbf
deepak <anandhadeepak@gmail.com>
2013-08-24 22:57:42 +0530

    deepak: changed home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 CleanMyCity::Application.routes.draw do
+  root :to => 'garbage_tickets#new'
   resources :garbage_tickets, only: [:new, :create]
   match '/garbage_tickets/visualize', to: "garbage_tickets#visualize", as: :visualize
 end
