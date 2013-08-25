c81adec911812addec9661b8771edd7c9b3a2393
Abhinaya <sabhinay@thoughtworks.com>
2013-08-25 10:18:42 +0530

    Abhi/Sharanya- colour coding by level and focus level

--- app/controllers/garbage_tickets_controller.rb
+++ app/controllers/garbage_tickets_controller.rb
@@ -1,5 +1,6 @@
 class GarbageTicketsController < ApplicationController
   require 'net/http'
+  require 'cgi'
 
   def show
     @garbage_ticket = GarbageTicket.find(params[:id])
@@ -20,17 +21,19 @@ class GarbageTicketsController < ApplicationController
   end
 
   def visualize
-    @lat_long_group = GarbageTicket.count(:all, group: [:latitude, :longitude])
+    @latitude = params[:location]["lat"]
+    @longitude = params[:location]["lng"]
+    @lat_long_group = GarbageTicket.count(:all, group: [:latitude, :longitude, :level])
   end
 
   def create
-    geocode_url = "http://maps.googleapis.com/maps/api/geocode/json?sensor=true&address=" + params[:garbage_ticket][:location]
+    geocode_url = "http://maps.googleapis.com/maps/api/geocode/json?sensor=true&address=" + CGI.escape(params[:garbage_ticket][:location])
     response_json = JSON.parse(Net::HTTP.get_response(URI.parse geocode_url).body)
     lat_lng = response_json["results"].first["geometry"]["location"]
 
     @garbage_ticket = GarbageTicket.new(params[:garbage_ticket].merge(latitude: lat_lng["lat"], longitude: lat_lng["lng"]))
     @garbage_ticket.save
-    redirect_to :visualize
+    redirect_to visualize_path(location: lat_lng)
   end
 
   def destroy
